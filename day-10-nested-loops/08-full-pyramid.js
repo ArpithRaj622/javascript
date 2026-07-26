0d4cd67968961b30cf067da83cf62470c8b03dec
Arpith622 <arpith.raj.622@gmail.com>
2026-07-26 19:53:26 +0530

    Modify file 08-full-pyramid.js

--- day-10-nested-loops/08-full-pyramid.js
+++ day-10-nested-loops/08-full-pyramid.js
@@ -1,6 +1,6 @@
 for (let i = 1; i <= 5; i ++) {
     for (let j = 1; j <= 5-i; j++) {
-        process.stdout.write("$");
+        process.stdout.write(" ");
     }
     for (let k = 1; k <= (2 * i) - 1; k++) {
         process.stdout.write("*");
