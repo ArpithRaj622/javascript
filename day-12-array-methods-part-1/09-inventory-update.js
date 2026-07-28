ed972d8a187e01e26e35ed612b35257905f964dc
Arpith Raj <arpith.raj.622@gmail.com>
2026-07-28 15:36:10 +0530

    Complete inventory update

---
+++ day-12-array-methods-part-1/09-inventory-update.js
@@ -0,0 +1,10 @@
+let products = ["Laptop", "Mouse", "Keyboard"];
+
+products.push("Monitor");
+products.shift();
+
+let totalProducts = products.length;
+
+console.log(products);
+console.log(`Total Products = ${totalProducts}`);
+
