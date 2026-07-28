16ec82c0315928bd667ec9a6cd81702c59a26db5
Arpith Raj <arpith.raj.622@gmail.com>
2026-07-28 14:52:57 +0530

    Complete remove first product

---
+++ day-12-array-methods-part-1/05-remove-first-product.js
@@ -0,0 +1,8 @@
+let products = ["Laptop", "Mouse", "Keyboard", "Monitor"];
+
+console.log(products);
+
+let removedProduct = products.shift();
+
+console.log(`Removed product = ${removedProduct}`);
+console.log(products);
