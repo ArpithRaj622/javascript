b16aef27b5cc5ce6668127054a828f2e3e43570f
Arpith Raj <arpith.raj.622@gmail.com>
2026-07-28 14:43:39 +0530

    Complete store discontinued product

---
+++ day-12-array-methods-part-1/04-store-discontinued-product.js
@@ -0,0 +1,8 @@
+let products = ["Laptop", "Mouse", "Keyboard", "Monitor"];
+
+console.log(products);
+
+let removedProduct = products.pop();
+
+console.log(`Removed Product = ${removedProduct}`);
+console.log(products);
