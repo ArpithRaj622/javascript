ab58dff6433c3897fadea8d26a052c37a97e960c
Arpith Raj <arpith.raj.622@gmail.com>
2026-07-28 21:36:07 +0530

    Complete copy product list

---
+++ day-12-array-methods-part-2/08-copy-product-list.js
@@ -0,0 +1,11 @@
+const products = ["Laptop", "Mouse", "Keyboard", "Monitor", "Printer"];
+
+const requiredProducts = products.slice(0,3);
+
+console.log(`Old list of products:
+${products}`);
+
+console.log();
+
+console.log(`New list of products:
+${requiredProducts}`);
