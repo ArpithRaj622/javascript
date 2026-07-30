2fb1d270892560db991f1d4fa5fea7c8edb1a470
Arpith Raj <arpith.raj.622@gmail.com>
2026-07-30 16:13:14 +0530

    Complete function with three parameters

---
+++ day-14-functions-part-1/07-function-with-three-parameters.js
@@ -0,0 +1,7 @@
+function studentDetails(name, age, course) {
+    console.log(`Student: ${name}`);
+    console.log(`Age: ${age}`);
+    console.log(`Course: ${course}`);
+}
+
+studentDetails("Arpith", 24, "BCA");
