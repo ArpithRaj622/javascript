0b7ae96d1f89a7796d4fce76117f6ffe5e3a0d12
Arpith Raj <arpith.raj.622@gmail.com>
2026-07-30 15:53:13 +0530

    Complete functions with multiple statements

---
+++ day-14-functions-part-1/03-function-with-multiple-statements.js
@@ -0,0 +1,8 @@
+function customerDashboard() {
+    console.log("Welcome back!");
+    console.log("Loading your profile...");
+    console.log("Loading your orders...");
+    console.log("Dashboard ready!");
+}
+
+customerDashboard();
