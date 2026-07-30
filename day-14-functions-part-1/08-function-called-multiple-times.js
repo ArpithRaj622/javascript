1539a96d6373a0b32370b28ef623b1cc32f2a86e
Arpith Raj <arpith.raj.622@gmail.com>
2026-07-30 16:18:16 +0530

    Complete function called multiple times

---
+++ day-14-functions-part-1/08-function-called-multiple-times.js
@@ -0,0 +1,9 @@
+function studentDetails(name, age, course) {
+    console.log(`Student: ${name}`);
+    console.log(`Age: ${age}`);
+    console.log(`Course: ${course}`);
+}
+
+studentDetails("Arpith", 24, "BCA");
+studentDetails("Rahul", 22, "B.Tech");
+studentDetails("Priya", 23, "MCA");
