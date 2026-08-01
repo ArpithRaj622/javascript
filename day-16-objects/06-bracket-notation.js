52cfb3b1066c853223fd8830d7305e06a16e0a62
Arpith Raj <arpith.raj.622@gmail.com>
2026-08-01 15:15:45 +0530

    Complete bracket notation

---
+++ day-16-objects/06-bracket-notation.js
@@ -0,0 +1,9 @@
+const book = {
+    title : "Atomic Habits",
+    author : "James Clear",
+    price : 499
+};
+
+console.log(book["title"]);
+console.log(book["author"]);
+console.log(book["price"]);
