9d8c47843c06cc4394a8e21806942dfb700b908c
Arpith Raj <arpith.raj.622@gmail.com>
2026-08-01 15:22:31 +0530

    Complete dot vs bracket notation

---
+++ day-16-objects/07-dot-vs-bracket-notation.js
@@ -0,0 +1,9 @@
+const movie = {
+    name : "Inception",
+    director : "Christopher Nolan",
+    year : 2010
+};
+
+console.log(movie.name);
+console.log(movie["director"]);
+console.log(movie.year);
