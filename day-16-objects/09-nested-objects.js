235e1acf794af3b90ac36eef30d146b1db19b3ea
Arpith Raj <arpith.raj.622@gmail.com>
2026-08-01 15:33:17 +0530

    Complete nested objects

---
+++ day-16-objects/09-nested-objects.js
@@ -0,0 +1,13 @@
+const employee = {
+    name : "Arpith",
+    age : 24,
+    address : {
+        city : "Raichur",
+        state : "Karnataka"
+    }
+};
+
+console.log(employee.name);
+console.log(employee.age);
+console.log(employee.address.city);
+console.log(employee.address.state);
