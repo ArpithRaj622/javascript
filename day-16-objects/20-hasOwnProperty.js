ea14b49aac672b6504004a4d41b384438ed00c4d
Arpith Raj <arpith.raj.622@gmail.com>
2026-08-01 20:38:23 +0530

    Complete hasOwnProperty

---
+++ day-16-objects/20-hasOwnProperty.js
@@ -0,0 +1,8 @@
+const mobile = {
+    brand : "Samsung",
+    model : "S25 Ultra",
+    storage : "512GB"
+}
+
+console.log(mobile.hasOwnProperty("brand"));
+console.log(mobile.hasOwnProperty("price"));
