9be1855e3349ec8996935e4ae533fcc9d16bcc20
Arpith Raj <arpith.raj.622@gmail.com>
2026-08-02 18:26:56 +0530

    Complete object destructuring

---
+++ day-17-advanced-objects/01-object-destructuring.js
@@ -0,0 +1,10 @@
+const employee = {
+    name : "Rahul",
+    department : "Engineering",
+    salary : 50000
+};
+
+const {department, salary} = employee;
+
+console.log(department);
+console.log(salary);
