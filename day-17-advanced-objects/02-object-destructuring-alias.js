9203d269fce477e626903e02b30dd8da92e0f0c7
Arpith Raj <arpith.raj.622@gmail.com>
2026-08-02 18:40:00 +0530

    Complete object destructuring alias

---
+++ day-17-advanced-objects/02-object-destructuring-alias.js
@@ -0,0 +1,10 @@
+const book = {
+    title : "Atomic Habits",
+    author : "James Clear",
+    price : 499
+};
+
+const {title : bookTitle, price : bookPrice} = book;
+
+console.log(bookTitle);
+console.log(bookPrice);
