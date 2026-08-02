ec476846ba626d5a21f2a9b8a0d91f7f4712cde4
Arpith Raj <arpith.raj.622@gmail.com>
2026-08-02 22:59:24 +0530

    Complete nested array destructuring

---
+++ day-17-advanced-objects/22-nested-array-destructuring.js
@@ -0,0 +1,7 @@
+const skills = ["HTML", ["CSS", "JavaScript"]];
+
+const [firstSkill, [secondSkill, thirdSkill]] = skills;
+
+console.log(firstSkill);
+console.log(secondSkill);
+console.log(thirdSkill);
