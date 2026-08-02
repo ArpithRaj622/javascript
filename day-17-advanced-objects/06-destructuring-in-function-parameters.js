075cd27ab5bb4c5f9bd25f2b150eb183657559b8
Arpith Raj <arpith.raj.622@gmail.com>
2026-08-02 19:46:39 +0530

    Complete destructuring in function parameters

---
+++ day-17-advanced-objects/06-destructuring-in-function-parameters.js
@@ -0,0 +1,11 @@
+const student = {
+    name : "Arpith",
+    course : "BCA"
+};
+
+function showStudent({name, course}) {
+    console.log(name);
+    console.log(course);
+}
+
+showStudent(student);
