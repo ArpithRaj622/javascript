fdeff81593418cd60d5f0ea9a813518b333b781b
Arpith Raj <arpith.raj.622@gmail.com>
2026-08-03 18:03:20 +0530

    Complete access object in array

---
+++ day-18-arrays-of-objects/02-access-object-in-array.js
@@ -0,0 +1,9 @@
+const students = [
+    {name: "Arpith", age: 24, course: "BCA"},
+    {name: "Raj", age: 23, course: "BCA"},
+    {name: "Arpith Raj", age: 26, course: "MCA" }
+];
+
+console.log(students[0].name);
+console.log(students[1].course);
+console.log(students[2].age);
