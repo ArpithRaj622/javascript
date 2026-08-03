424233c96ee0d255c5d584e1dcf6f9796f3874cc
Arpith Raj <arpith.raj.622@gmail.com>
2026-08-03 17:40:25 +0530

    Complete create array of objects

---
+++ 01-create-array-of-objects.js
@@ -0,0 +1,7 @@
+const students = [
+    {name: "Arpith", age: 24, course: "BCA"},
+    {name: "Raj", age: 23, course: "BCA"},
+    {name: "Arpith Raj", age: 26, course: "MCA" }
+];
+
+console.log(students);
