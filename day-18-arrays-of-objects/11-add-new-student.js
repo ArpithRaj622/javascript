d16b4bb122460a636a8cde22e10788acc3bc2d52
Arpith Raj <arpith.raj.622@gmail.com>
2026-08-03 20:42:34 +0530

    Complete add new student

---
+++ day-18-arrays-of-objects/11-add-new-student.js
@@ -0,0 +1,9 @@
+const students = [
+    {name: "Arpith", age: 24, course: "BCA"},
+    {name: "Raj", age: 23, course: "BCA"},
+    {name: "Arpith Raj", age: 26, course: "MCA" }
+];
+
+students.push({name : "Kiran", age: "22", course: "BCA"});
+
+console.log(students);
