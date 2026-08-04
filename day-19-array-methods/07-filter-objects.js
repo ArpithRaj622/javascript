02d6dd8c5fec6d6441e204896e000a3a6f0ef269
Arpith Raj <arpith.raj.622@gmail.com>
2026-08-04 20:33:51 +0530

    Complete filter object

---
+++ day-19-array-methods/07-filter-objects.js
@@ -0,0 +1,12 @@
+const students = [
+    { name: "Arpith", age: 24, course: "BCA" },
+    { name: "Raj", age: 23, course: "MCA" },
+    { name: "Sam", age: 25, course: "BCA" },
+    { name: "Tom", age: 22, course: "MBA" }
+];
+
+const bcaStudents = students.filter((student) => {
+    return student.course === "BCA";
+});
+
+console.log(bcaStudents);
