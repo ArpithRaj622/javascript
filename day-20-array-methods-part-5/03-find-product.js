0a3e6c5ba5b20f8c85b9a02bfed5f702b41d1eeb
Arpith Raj <arpith.raj.622@gmail.com>
2026-08-05 19:58:00 +0530

    Complete find product

---
+++ day-20-array-methods-part-5/03-find-product.js
@@ -0,0 +1,12 @@
+const products = [
+    { name: "Laptop", price: 90000 },
+    { name: "Headphones", price: 9000 },
+    { name: "Mouse", price: 2000 },
+    { name: "PlayStation 5", price: 50000 }
+];
+
+const findMouse = products.find((product) => {
+    return product.name === "Mouse";
+});
+
+console.log(findMouse);
