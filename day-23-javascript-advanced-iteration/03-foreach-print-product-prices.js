e2ac766a2a4db47d1ca686ea33ecb95e25c51332
Arpith Raj <arpith.raj.622@gmail.com>
2026-08-08 18:13:30 +0530

    Complete foreach print product prices

---
+++ day-23-javascript-advanced-iteration/03-foreach-print-product-prices.js
@@ -0,0 +1,10 @@
+const products = [
+    { name: "Laptop", price: 90000 },
+    { name: "Mouse", price: 2000 },
+    { name: "Keyboard", price: 3500 },
+    { name: "Monitor", price: 15000 }
+];
+
+products.forEach((product) => {
+    console.log(product.price);
+});
