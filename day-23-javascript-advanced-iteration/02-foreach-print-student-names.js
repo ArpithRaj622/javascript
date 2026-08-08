ebb19acf364467b4eb42a33d2822eb3629850979
Arpith Raj <arpith.raj.622@gmail.com>
2026-08-08 18:11:42 +0530

    Complete foreach print student names

---
+++ day-23-javascript-advanced-iteration/02-foreach-print-student-names.js
@@ -0,0 +1,10 @@
+const students = [
+    { name: "Arpith", course: "BCA" },
+    { name: "Raj", course: "MCA" },
+    { name: "Sam", course: "MBA" },
+    { name: "Tom", course: "BCA" }
+];
+
+students.forEach((student) => {
+    console.log(student.name);
+});
