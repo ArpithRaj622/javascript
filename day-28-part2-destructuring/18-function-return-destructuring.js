0490d961b3a047ec40dbd374d9605dd8db67c3f5
Arpith Raj <arpith.raj.622@gmail.com>
2026-08-13 22:26:15 +0530

    Complete function return destructuring

---
+++ day-28-part2-destructuring/18-function-return-destructuring.js
@@ -0,0 +1,11 @@
+function getUser() {
+    return {
+        name : "Arpith",
+        role : "Frontend Developer"
+    }
+}
+
+const {name, role} = getUser();
+
+console.log(name);
+console.log(role);
