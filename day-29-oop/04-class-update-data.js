61d4cfae049c572565c8d394cf7f786d857ac42b
Arpith Raj <arpith.raj.622@gmail.com>
2026-08-14 13:39:59 +0530

    Complete class update data

---
+++ day-29-oop/04-class-update-data.js
@@ -0,0 +1,15 @@
+class BankAccount {
+    constructor(owner, balance) {
+        this.owner = owner;
+        this.balance = balance
+    }
+
+    deposit(amount) {
+        this.balance += amount;
+        console.log(this.balance);
+    }
+}
+
+const account1 = new BankAccount("Arpith", 10000);
+
+account1.deposit(5000);
