462208651eff650c8a7fe4435b650e6b3eb69b10
Arpith Raj <arpith.raj.622@gmail.com>
2026-08-21 18:18:58 +0530

    Complete complete task

---
+++ day-33-event-delegation/06-complete-task/script.js
@@ -0,0 +1,7 @@
+const taskList = document.querySelector(".task-list");
+
+taskList.addEventListener("click", (event) => {
+    if (event.target.tagName === "BUTTON" && event.target.textContent === "Complete") {
+        event.target.parentElement.classList.add("completed");
+    }
+});
