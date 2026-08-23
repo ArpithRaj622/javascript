ca8c24e4b517b670768915783a3ef64f4280ab43
Arpith Raj <arpith.raj.622@gmail.com>
2026-08-23 22:20:07 +0530

    Complete promise all

---
+++ day-35-async-javascript/16-promise-all/script.js
@@ -0,0 +1,31 @@
+function getWeather() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Weather received");
+        }, 2000);
+    });
+}
+
+function getTemperature() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Temperature received");
+        }, 2000);
+    });
+}
+
+function getHumidity() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Humidity received");
+        }, 2000);
+    });
+}
+
+Promise.all([getWeather(), getTemperature(), getHumidity()
+])
+    .then((results) => {
+        results.forEach((result) => {
+            console.log(result);
+        });
+    });
