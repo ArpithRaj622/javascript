63cf7ab7ea52f1f909994136743d80b417ae2609
Arpith Raj <arpith.raj.622@gmail.com>
2026-08-23 20:29:40 +0530

    Complete promise task

---
+++ day-35-async-javascript/11-promise-task/script.js
@@ -0,0 +1,15 @@
+function getWeather() {
+    return new Promise((resolve, rejsect) => {
+        setTimeout(() => {
+            resolve("Weather data received");
+        }, 2000);
+    });
+}
+
+getWeather()
+    .then((result) =>{
+        console.log(result);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
