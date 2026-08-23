b8eef5bd9398582ff87043684e3ddbefc279822c
Arpith Raj <arpith.raj.622@gmail.com>
2026-08-23 13:16:51 +0530

    Complete timeout order

---
+++ day-35-async-javascript/02-timeout-order/script.js
@@ -0,0 +1,13 @@
+console.log("Start");
+
+setTimeout(() => {
+    console.log("Message 1");
+}, 3000);
+
+setTimeout(() => {
+    console.log("Message 2");
+}, 1000);
+
+setTimeout(() => {
+    console.log("Message 3");
+}, 2000);
