22e1b6699b7ba0b7ac2aafe6eb9793dc8a5099b5
Arpith Raj <arpith.raj.622@gmail.com>
2026-08-23 22:44:48 +0530

    Complete promise all settled

---
+++ day-35-async-javascript/18-promise-all-settled/script.js
@@ -0,0 +1,31 @@
+function getWeather() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Weather received");
+        }, 2000);
+    });
+}
+
+function getTemperature() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject("Temperature unavailable");
+        }, 2000);
+    });
+}
+
+function getHumidity() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Humidity received");
+        }, 2000);
+    });
+}
+
+Promise.allSettled([getWeather(), getTemperature(), getHumidity()
+])
+    .then((results) => {
+        results.forEach((result) => {
+            console.log(result);
+        });
+    })
