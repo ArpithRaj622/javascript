139af78977e75b0b38c6dd8f6513f26d25e26e77
Arpith Raj <arpith.raj.622@gmail.com>
2026-08-26 16:46:36 +0530

    Complete access api data

---
+++ day-37-fetch-api/03-access-api-data/script.js
@@ -0,0 +1,8 @@
+async function getPost() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+    const data = await response.json();
+    console.log(data.title);
+    console.log(data.body);
+}
+
+getPost();
