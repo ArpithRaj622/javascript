f672c23bb7deea54b207bea619e18fa8cd4f9e26
Arpith Raj <arpith.raj.622@gmail.com>
2026-08-26 17:23:26 +0530

    Complete fetch multiple data

---
+++ day-37-fetch-api/04-fetch-multiple-data/script.js
@@ -0,0 +1,9 @@
+async function getPosts() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const posts = await response.json();
+    posts.forEach((post) => {
+        console.log(post.title);
+    });
+}
+
+getPosts();
