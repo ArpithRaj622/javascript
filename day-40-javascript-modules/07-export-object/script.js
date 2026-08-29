0811e84fae14cc5d1bd06313e81306cfe144cf99
Arpith Raj <arpith.raj.622@gmail.com>
2026-08-29 21:10:20 +0530

    Complete export object

---
+++ day-40-javascript-modules/07-export-object/script.js
@@ -0,0 +1,5 @@
+import { user } from "./user.js";
+
+console.log(user.name);
+console.log(user.age);
+console.log(user.role);
---
+++ day-40-javascript-modules/07-export-object/user.js
@@ -0,0 +1,7 @@
+const user = {
+    name : "Arpith",
+    age : 24,
+    role : "Frontend Developer",
+}
+
+export { user };
