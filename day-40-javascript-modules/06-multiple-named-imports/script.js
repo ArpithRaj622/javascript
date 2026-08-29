94fbb95aaaa945d800e841a8ee48ee9199c3c13b
Arpith Raj <arpith.raj.622@gmail.com>
2026-08-29 21:05:28 +0530

    Complete multiple imports

---
+++ day-40-javascript-modules/06-multiple-named-imports/script.js
@@ -0,0 +1,9 @@
+import { getName, getAge, getCity } from "./user.js";
+
+const name = getName();
+const age = getAge();
+const city = getCity();
+
+console.log(name);
+console.log(age);
+console.log(city);
---
+++ day-40-javascript-modules/06-multiple-named-imports/user.js
@@ -0,0 +1,13 @@
+function getName() {
+    return "Arpith";
+}
+
+function getAge() {
+    return 24;
+}
+
+function getCity() {
+    return "Raichur";
+}
+
+export { getName, getAge, getCity};
