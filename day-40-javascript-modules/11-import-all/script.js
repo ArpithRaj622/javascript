0cf182f5942e6e351fb691397efc8646ea4998a3
Arpith Raj <arpith.raj.622@gmail.com>
2026-08-29 21:45:41 +0530

    Complete import all

---
+++ day-40-javascript-modules/11-import-all/script.js
@@ -0,0 +1,9 @@
+import * as math from "./math.js";
+
+const sum = math.add(10, 5);
+const diff = math.subtract(10, 5);
+const product = math.multiply(10, 5);
+
+console.log(sum);
+console.log(diff);
+console.log(product);
