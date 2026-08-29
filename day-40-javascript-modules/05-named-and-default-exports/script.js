11bc12c0577c9f6a2f6d6c661a9df9a46a9bf67c
Arpith Raj <arpith.raj.622@gmail.com>
2026-08-29 20:58:26 +0530

    Complete name and default exports

---
+++ day-40-javascript-modules/05-named-and-default-exports/script.js
@@ -0,0 +1,5 @@
+import greet from "./user.js";
+import { getUserRole } from "./user.js";
+
+console.log(greet());
+console.log(getUserRole());
---
+++ day-40-javascript-modules/05-named-and-default-exports/user.js
@@ -0,0 +1,10 @@
+function greetUser() {
+    return "Hello user";
+}
+
+function getUserRole() {
+    return "Frontend Developer";
+}
+
+export default greetUser;
+export { getUserRole };
