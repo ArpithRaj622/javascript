8de8f8749247d07fa2024b58c09c146988e3d5c8
Arpith Raj <arpith.raj.622@gmail.com>
2026-09-01 12:04:07 +0530

    Complete object destructuring

---
+++ day-42-javascript-revision/08-object-destructuring/script.js
@@ -0,0 +1,12 @@
+const user = {
+    name: "Arpith",
+    age: 24,
+    city: "Raichur",
+    role: "Frontend Developer"
+};
+
+const {name, age,role} = user;
+
+console.log(name);
+console.log(age);
+console.log(role);
