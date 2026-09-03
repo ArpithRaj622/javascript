4c7d848c9439ae8997f090f2cca7e1330e7b38bc
Arpith Raj <arpith.raj.622@gmail.com>
2026-09-03 15:09:22 +0530

    Complete includes indexof

---
+++ day-42-javascript-revision/17-includes-indexof/script.js
@@ -0,0 +1,7 @@
+const skills = ["HTML", "CSS", "JavaScript", "React", "Node.js"];
+
+const reactExists = skills.includes("React");
+const javaScriptIndex = skills.indexOf("JavaScript");
+
+console.log(reactExists);
+console.log(javaScriptIndex);
