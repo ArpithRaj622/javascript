7b4479b8ae50359b52aec2d344bca7e629a5b5be
Arpith Raj <arpith.raj.622@gmail.com>
2026-09-05 19:51:35 +0530

    Complete object keys length

---
+++ day-42-javascript-revision/30-object-keys-length/script.js
@@ -0,0 +1,11 @@
+const product = {
+  name: "Laptop",
+  price: 50000,
+  brand: "Lenovo",
+  stock: 5,
+  category: "Electronics"
+};
+
+const prodPropertiesLength = Object.keys(product).length;
+
+console.log(prodPropertiesLength);
